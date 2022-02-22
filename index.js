2be4d1923d470dd9b516d6012d1907250fa3efac
Michael Chung <mikechung89@gmail.com>
2022-02-22 13:23:22 -0500

    commit tree 14517862d6d82a2db2cac09f056fbefe474226ad

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+// 1st test
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2
+
+// 2nd test
+const random = Math.ceil(Math.random() * 10);
+
+// 3rd test
+
+const num3 = 14;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(5,6,9,15,20);
